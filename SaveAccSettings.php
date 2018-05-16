80574623055ab8cff5b4da3fae7eea9184523785
ldmerasp <37580081+ldmerasp@users.noreply.github.com>
2018-05-16 17:27:37 +0300

    Add files via upload

---
+++ SaveAccSettings.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+    $con = mysqli_connect("localhost", "root", "", "ldme");
+
+    
+    $email = $_POST["email"];
+    
+        $firstname = $_POST["firstname"];
+
+        $lastname = $_POST["lastname"];
+
+        $phone = $_POST["phone"];
+
+    
+    $statement = "UPDATE user SET firstname = '$firstname', lastname = '$lastname', phone = '$phone' WHERE email = '$email'";
+
+    $response = array();
+
+    $response["success"] = false;
+    if(mysqli_query($con, $statement)){
+         $response["success"] = true;
+     }
+  
+  echo json_encode($response);
+?>
